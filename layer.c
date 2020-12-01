68d06763a3140967c08114de012d057655815eba
Devon Hastings <devonhastings810@gmail.com>
2020-11-30 23:15:47 -0500

    Continued work on the layer structure. I am still playing with the design of the structure and it's relationship to input data.

--- layer.c
+++ layer.c
@@ -10,7 +10,38 @@
 // TODO: I need this layer function to generate the matrices for the weights and biases
 // TODO: I think I will set it up so that I generate the weight based on the previous layer
 // TODO: dimensions. The bias will be trivial.
-layer *newLayer(int numberOfNodes)
+
+// TODO: Perhaps
+layer *newInputLayer(struct Layer **networkStart, int numberOfNodes)
 {
+    // Make sure the layer is nonzero or less
+    if (numberOfNodes <= 0) return NULL;
+
+    // Allocate memory or the layer
+    layer *new_input_layer = (layer*)malloc(sizeof(layer));
+    // Indicate the layer is an input layer
+    new_input_layer->inputLayer = 1;
+
+
+    // Set the previous pointer to a null
+    new_input_layer->prev = NULL;
+
+    // Hmmm
+
+
+
+}
+
+
+layer *newLayer(int numberOfNodes, void (*actFunc)(double*))
+{
+    // Test to make sure the number of nodes is greater than 0
+    if (numberOfNodes <= 0) return NULL;
+    // Allocate memory for the layer
+    layer *new_layer = (layer*)malloc(sizeof(layer));
+    // Indicate the layer is a not an input layer
+    new_layer->inputLayer = 0;
+
+
 
 }
--- layer.h
+++ layer.h
@@ -11,6 +11,8 @@ typedef struct Layer
     void (*activationFunction)(double*);
     struct Layer *prev;
     struct Layer *next;
+    // Should I include an input style flag? To indicate if the layer is an input?
+    int inputLayer;
 }layer;
 
 #endif //TESTPROJ_LAYER_H
